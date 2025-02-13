8f55f217a0ece6a71651d27d6546167f5333f2b6
king-beard <chucho_herrera10@icloud.com>
2025-02-12 22:00:06 -0600

    creating list categories

--- src/GiftExpertApp.jsx
+++ src/GiftExpertApp.jsx
@@ -1,9 +1,15 @@
-import { Fragment } from "react"
+import { Fragment, useState } from "react"
 
 export const GiftExpertApp = () => {
+
+  const [categories, setCategories] = useState(['Baki', 'Dragon Ball Z'])
+
   return (
     <Fragment>
       <h1>GiftExpertApp</h1>
+      <ol>
+        { categories.map((category, i) => <li key={category}>{ category }</li>)  }
+      </ol>
     </Fragment>
   )
 }
