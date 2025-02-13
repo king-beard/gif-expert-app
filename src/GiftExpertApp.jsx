63a292dace4df8a4edfec72a0e35e9ba3a80b1fa
king-beard <chucho_herrera10@icloud.com>
2025-02-12 22:14:09 -0600

    add new category

--- src/GiftExpertApp.jsx
+++ src/GiftExpertApp.jsx
@@ -4,11 +4,14 @@ export const GiftExpertApp = () => {
 
   const [categories, setCategories] = useState(['Baki', 'Dragon Ball Z'])
 
+  const OnAddCategory = () => setCategories([...categories, 'One Piece'])
+
   return (
     <Fragment>
       <h1>GiftExpertApp</h1>
+      <button onClick={ OnAddCategory }>Add new category</button>
       <ol>
-        { categories.map((category, i) => <li key={category}>{ category }</li>)  }
+        { categories.map((category, i) => <li key={`${category}-${i}`}>{ category }</li>)  }
       </ol>
     </Fragment>
   )
