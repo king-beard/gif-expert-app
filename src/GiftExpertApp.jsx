8a0e81e17573cb789d2474b0a7628a30a25fbd8a
king-beard <chucho_herrera10@icloud.com>
2025-02-16 22:56:08 -0600

    new component GifGrid

--- src/GiftExpertApp.jsx
+++ src/GiftExpertApp.jsx
@@ -1,5 +1,6 @@
 import { Fragment, useState } from "react"
 import { AddCategory } from "./components/AddCategory"
+import { GifGrid } from "./components/GifGrid"
 
 export const GiftExpertApp = () => {
 
@@ -15,7 +16,7 @@ export const GiftExpertApp = () => {
       <h1>GiftExpertApp</h1>
       <AddCategory onNewCategory={ value => onAddCategory(value) } />
       <ol>
-        { categories.map((category, i) => <li key={`${category}-${i}`}>{ category }</li>)  }
+        { categories.map((category, i) => <GifGrid key={`${category}-${i}`} category={ category } /> ) }
       </ol>
     </Fragment>
   )
---
+++ src/components/GifGrid.jsx
@@ -0,0 +1,9 @@
+import { Fragment } from 'react'
+
+export const GifGrid = ({ category }) => {
+  return (
+    <Fragment>
+      <h3>{ category }</h3>
+    </Fragment>
+  )
+}
