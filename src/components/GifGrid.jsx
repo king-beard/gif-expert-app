ec12ec1df6f6ca9059842f50b903c086f9a1892d
king-beard <chucho_herrera10@icloud.com>
2025-02-17 22:14:06 -0600

    show Loading

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -9,7 +9,9 @@ export const GifGrid = ({ category }) => {
   return (
     <Fragment>
       <h3>{category}</h3>
-
+      {
+        isLoading && <h2>Loading.....</h2>
+      }
       <div className="card-grid">
         {images.map((img) => <GifItem key={img.id} image={img} />)}
       </div>
